7514ff1473f4ae344115a809c429a60949c96732
CaramelHeaven <srg.fominov@gmail.com>
2019-06-11 00:43:15 +0400

    cleaned up

--- Lennach/App/Domain/RemoteMainMapper.swift
+++ Lennach/App/Domain/RemoteMainMapper.swift
@@ -76,68 +76,12 @@ class RemoteMainMapper {
     func mapResponseToAllBoards(response: AllBoardsResponse) -> AllBoards {
         var allBoards = AllBoards()
 
-        //Below we search all arrays and add each element to common array inside allBoards variable
-//        for item in response.users! {
-//            allBoards.boards.append(BoardDescription(name: item.name!, bumpLimit: item.bumpLimit!, id: item.id!))
-//        }
-
         for item in response.subjects! {
-            switch item.id! {
-            case "bi":
-                allBoards.boards.append(BoardDescription(name: "Отдельные треды \"посоветуйте велосипед за 15к\" нежелательны, пишите в посоветуй-тред. Конференция доски в Телеграме - @velach", bumpLimit: item.bumpLimit!, id: item.id!))
-                break
-            case "fl":
-                allBoards.boards.append(BoardDescription(name: "Обсуждаем теоретические и практические аспекты иностранных языков, делимся своим опытом.", bumpLimit: item.bumpLimit!, id: item.id!))
-                break
-            case "me":
-                allBoards.boards.append(BoardDescription(name: "/me - это раздел для обсуждения медицины, здоровья, терапии, диагностики, здорового образа жизни.", bumpLimit: item.bumpLimit!, id: item.id!))
-                break
-            case "mo":
-                allBoards.boards.append(BoardDescription(name: "Доска для помощи как начинающим мотоциклистам, так и опытным байкерам. Разговоры на все темы, так или иначе связанные с мотоциклами и байкерским движением. У нас тут мотобратство!", bumpLimit: item.bumpLimit!, id: item.id!))
-                break
-            case "tv":
-                allBoards.boards.append(BoardDescription(name: "Доска посвящена обсуждению телесериалов, телешоу, телепрограмм и всего, связанного с ними. Вся анимация следует в /c/, все фильмы - в /mov/. Закрепленный тред предназначен исключительно для реквестов и помощи в поисках. Конференция доски в Телеграме - @ru2chmov", bumpLimit: item.bumpLimit!, id: item.id!))
-                break
-            case "wm":
-                allBoards.boards.append(BoardDescription(name: "Обсуждение военной техники, армий и вооруженных конфликтов. Политика, а также новости, не связанные с военной техникой обсуждаются в /po/ и /news/. Оружие самообороны, камуфляж, ножи и т.д. - /w/.", bumpLimit: item.bumpLimit!, id: item.id!))
-                break
-            default:
-                print("asf")
-            }
-
+            allBoards.boards.append(BoardDescription(name: item.name!, bumpLimit: item.bumpLimit!, id: item.id!))
         }
-
-        for item in response.software! {
-            switch item.id! {
-            case "gd":
-                allBoards.boards.append(BoardDescription(name: "Доска создана для обсуждения вопросов, связаных с разработкой компьютерных игр. Аргументированная критика поощряется.", bumpLimit: item.bumpLimit!, id: item.id!))
-                break
-            case "hw":
-                allBoards.boards.append(BoardDescription(name: "Обсуждаем компьютерное железо.", bumpLimit: item.bumpLimit!, id: item.id!))
-                break
-            case "moni":
-                allBoards.boards.append(BoardDescription(name: "Доска для мобильных телефонов. О покупке и мелких вопросах спрашивают прикрепленном треде. Читалки - в /bo/, наушники и плееры - в /t/.", bumpLimit: item.bumpLimit!, id: item.id!))
-                break
-            case "pr":
-                allBoards.boards.append(BoardDescription(name: "Программы и операционные системы обсуждаются в /s/. Воздержитесь от холиваров. Конференция доски в Телеграме - @pr2ch", bumpLimit: item.bumpLimit!, id: item.id!))
-                break
-            case "ra":
-                allBoards.boards.append(BoardDescription(name: "Паяем, осуждаем друг друга за разводку платы и макетку из куска фанеры. Пиратское радио и официальное - связисты приветствуются. Не можешь сходу назвать закон ома и первое правило кирхгофа - в прикрепленный тред по любому вопросу.", bumpLimit: item.bumpLimit!, id: item.id!))
-                break
-            case "t":
-                allBoards.boards.append(BoardDescription(name: "t/ - доска для обсуждения техники и технологий. Мобильные телефоны, планшеты, гаджеты обсуждаются в /mobi/. Компьютерное железо обсуждается в /hw/, но при этом по вопросам выбора или починки ноутбука можно обратиться сюда, по вопросам компьютерной периферии тоже, так повелось.", bumpLimit: item.bumpLimit!, id: item.id!))
-                break
-            default:
-                print("asf")
-            }
-        }
-
-//        for item in response.forAdults! {
-//            allBoards.boards.append(BoardDescription(name: item.name!, bumpLimit: item.bumpLimit!, id: item.id!))
-//        }
 
         for item in response.differences! {
-            allBoards.boards.append(BoardDescription(name: "", bumpLimit: item.bumpLimit!, id: item.id!))
+            allBoards.boards.append(BoardDescription(name: item.name!, bumpLimit: item.bumpLimit!, id: item.id!))
         }
 
         for item in response.creation! {
@@ -208,7 +152,7 @@ extension RemoteMainMapper {
             }
         }
 
-        print("ARRAY OF REPLIES: \(arrayReplies)")
+        
         return arrayReplies
     }
 }
